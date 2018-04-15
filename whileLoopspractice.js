e2ea8e2314df513af8b0971372f2bf965aa13234
Joel Okoro <jukachi95@gmail.com>
2018-04-15 21:04:21 +0100

    Initial while loops with exercise on multiples

---
+++ whileLoopspractice.js
@@ -0,0 +1,42 @@
+// alert("Connected");
+
+var count = 1;
+
+while(count <= 10){
+	console.log(count)
+	count+=2;
+}
+
+// Multipls of Four
+
+var multiple = 4;
+
+while(multiple <= 24){
+	if(multiple % 4 === 0){
+		console.log(multiple);
+	}
+	multiple++;
+}
+
+// Multiples of 3
+
+var threeMultiple = 3;
+
+while(threeMultiple <= 30){
+	if(threeMultiple % 3 ===0){
+		console.log(threeMultiple);
+	}
+	threeMultiple++;
+}
+
+// Even Numbers
+
+var even = 2;
+
+while(even <= 46){
+	if(even % 2 === 0){
+		console.log(even);
+	}
+	even++;
+}
+
