c7d3e147b19d99f49ccb0348a78f2add6ece84f2
Joel Okoro <jukachi95@gmail.com>
2018-04-18 21:28:20 +0100

    Additional Exercises

--- whileLoopspractice.js
+++ whileLoopspractice.js
@@ -1,4 +1,5 @@
 // alert("Connected");
+console.log("Print Odd Numbers version 1");
 
 var count = 1;
 
@@ -8,6 +9,7 @@ while(count <= 10){
 }
 
 // Multipls of Four
+console.log("Print multiples of Four");
 
 var multiple = 4;
 
@@ -19,6 +21,7 @@ while(multiple <= 24){
 }
 
 // Multiples of 3
+console.log("Print Multiples of 3");
 
 var threeMultiple = 3;
 
@@ -30,6 +33,8 @@ while(threeMultiple <= 30){
 }
 
 // Even Numbers
+console.log("Print Even Numbers");
+
 
 var even = 2;
 
@@ -40,3 +45,39 @@ while(even <= 46){
 	even++;
 }
 
+
+// -10 to 19
+console.log("Print from -10 to 19");
+
+
+var negative = -10;
+
+while(negative <= 19){
+	console.log(negative);
+	negative++;
+}
+
+// Print odd numbers between 300 and 333
+
+console.log("Print odd nunbers between 300 and 333");
+
+var threeHundred = 300;
+
+while(threeHundred <= 333){
+	if(threeHundred % 2 !== 0){
+		console.log(threeHundred);
+	}
+	threeHundred++;
+}
+
+// Print numbers divisible by 3 and 5 that are between 5 and 50
+console.log("Numbers that are divisible by three and five that are between 5 and 50");
+
+var fiveMultiple = 5;
+
+while(fiveMultiple <= 50){
+	if(fiveMultiple % 3 === 0 && fiveMultiple % 5 === 0){
+		console.log(fiveMultiple);
+	}
+	fiveMultiple++;
+}
