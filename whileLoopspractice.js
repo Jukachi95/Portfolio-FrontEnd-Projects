dab8e0bedf7d520debeb5ea4c6629fd856f987c0
Joel Okoro <jukachi95@gmail.com>
2018-04-29 19:56:19 +0100

    Additional practice

--- whileLoopspractice.js
+++ whileLoopspractice.js
@@ -1,83 +1,56 @@
-// alert("Connected");
-console.log("Print Odd Numbers version 1");
+console.log("While Loop that prints all odd numbers under 10")
 
-var count = 1;
+var counter = 0;
 
-while(count <= 10){
-	console.log(count)
-	count+=2;
-}
-
-// Multipls of Four
-console.log("Print multiples of Four");
-
-var multiple = 4;
-
-while(multiple <= 24){
-	if(multiple % 4 === 0){
-		console.log(multiple);
+while(counter <= 10){
+	if(counter % 2 !== 0){
+		console.log(counter);
 	}
-	multiple++;
+	counter++;
 }
 
-// Multiples of 3
-console.log("Print Multiples of 3");
+console.log("While Loop that only prints multiples of 4 in the console log *starting from 4*");
 
-var threeMultiple = 3;
+var counter = 4;
 
-while(threeMultiple <= 30){
-	if(threeMultiple % 3 ===0){
-		console.log(threeMultiple);
+while(counter <= 48){
+	if(counter % 4 === 0){
+		console.log(counter)
 	}
-	threeMultiple++;
+	counter++;
 }
 
-// Even Numbers
-console.log("Print Even Numbers");
+console.log("While Loop that only prints multiples of 3 in the console log *starting from 3*");
 
+var counter = 3;
 
-var even = 2;
-
-while(even <= 46){
-	if(even % 2 === 0){
-		console.log(even);
+while(counter <= 38){
+	if(counter % 3 === 0){
+		console.log(counter);
 	}
-	even++;
+	counter++;
 }
 
+console.log("A while loop that prints only even numbers");
 
-// -10 to 19
-console.log("Print from -10 to 19");
+var counter = 0;
 
-
-var negative = -10;
-
-while(negative <= 19){
-	console.log(negative);
-	negative++;
-}
-
-// Print odd numbers between 300 and 333
-
-console.log("Print odd nunbers between 300 and 333");
-
-var threeHundred = 300;
-
-while(threeHundred <= 333){
-	if(threeHundred % 2 !== 0){
-		console.log(threeHundred);
+while(counter <= 44){
+	if(counter % 2 === 0){
+		console.log(counter);
 	}
-	threeHundred++;
+	counter++;
 }
 
-// Print numbers divisible by 3 and 5 that are between 5 and 50
-console.log("Numbers that are divisible by three and five that are between 5 and 50");
 
-var fiveMultiple = 5;
 
-while(fiveMultiple <= 50){
-	if(fiveMultiple % 3 === 0 && fiveMultiple % 5 === 0){
-		console.log(fiveMultiple);
+console.log("A while loop that prints numbers divisible by 3 & 5 that are under 50 ");
+
+var counter = 3;
+
+while(counter <= 50){
+	if(counter % 3 === 0 && counter % 5 === 0){
+		console.log(counter)
 	}
-	fiveMultiple++;
+	counter++;
 }
